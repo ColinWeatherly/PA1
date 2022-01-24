f1b800eadf41d628935af2d03b03f268409026c1
ColinWeatherly <colrweat@ut.utm.edu>
2022-01-23 23:37:47 -0600

    Made progress on my abstract Animal class.

---
+++ CSCI_352_PA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSCI_352_PA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
