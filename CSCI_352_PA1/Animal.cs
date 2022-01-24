f2b7cb6771efed35eae0e720961ccaa2282a0ffc
ColinWeatherly <colrweat@ut.utm.edu>
2022-01-23 23:38:58 -0600

    Made progress on my abstract Animal class.

---
+++ CSCI_352_PA1/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSCI_352_PA1
+{
+    public abstract class Animal
+    {
+        private Animal()
+        {
+            string Name = "Default";
+            int Age = 1;
+            string Noise = "GRAWR";
+            int Weight = 100;
+        }
+
+        public abstract void printInfo();
+
+        public abstract void makeNoise();
+
+        public abstract void ageUp();
+
+    }
+}
